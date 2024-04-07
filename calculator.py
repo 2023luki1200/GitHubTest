3b0a3acb20964f8e775157307d605ff2b73f5f96
2023luki1200 <lukas.kiener@gmail.com>
2024-04-07 17:29:13 +0200

    Function add, sub, mult, div, mod

---
+++ calculator.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  7 17:19:15 2024
+
+@author: lukas
+"""
+
+def add(a, b):
+    
+    return a+b
+
+def sub(a, b):
+    return a-b
+
+def mult(a, b):
+    return a*b
+
+def div(a, b):
+    return a/b
+
+def mod (a,b):
+    return a%b
+
+
+print(mod(7,4))
