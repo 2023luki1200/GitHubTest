ff238d723750bef377e2d9b14d9f9c34c6573d92
2023luki1200 <lukas.kiener@gmail.com>
2024-04-08 20:25:20 +0200

    test ruff

--- calculator.py
+++ calculator.py
@@ -6,10 +6,10 @@ Created on Sun Apr  7 17:19:15 2024
 """
 
 def add(a, b):  #add a and b (a+b) and returns the sum
-    return a+b
+    return a +b
 
 def sub(a, b): #subtract b from a and returns it
-    return a-b
+    return a - b
 
 def mult(a, b): #multiply a and b and returns the result
     return a*b
