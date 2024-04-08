caaaa3c8e207d30e3be12bd085d31dc28d8a2a56
2023luki1200 <lukas.kiener@gmail.com>
2024-04-08 21:57:50 +0200

    fourth

--- calculator.py
+++ calculator.py
@@ -1,3 +1,22 @@
-def add(a, b):
-    """Add two numbers together"""
-    return a + b
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  7 17:19:15 2024
+
+
+@author: lukas
+"""
+
+def add(a, b):  #add a and b (a+b) and returns the sum
+    return a +b
+
+def sub(a, b): #subtract b from a and returns it
+    return a - b
+
+def mult(a, b): #multiply a and b and returns the result
+    return a * b
+
+def div(a, b): #divide a/b and returns the result
+    return a / b
+ 
+
+
