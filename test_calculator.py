4c7038ccb91c57b5620d2c8feac362fd9cd7fe5d
2023luki1200 <lukas.kiener@gmail.com>
2024-04-08 22:45:00 +0200

    format4

--- test_calculator.py
+++ test_calculator.py
@@ -12,25 +12,25 @@ from calculator import div
 from calculator import mult
 
 
-def test_add():  # test for the add function
+def test_add():
     assert add(1, 1) == 2
     assert add(-3, 5) == 2
     assert add(-1, -4) == -5
 
 
-def test_sub():  # test for the sub function
+def test_sub():
     assert sub(1, 1) == 0
     assert sub(-3, 5) == -8
     assert sub(-1, -4) == 3
 
 
-def test_mult():  # Test for the multiply function
+def test_mult():
     assert mult(1, 1) == 1
     assert mult(-3, 5) == -15
     assert mult(-1, -4) == 4
 
 
-def test_div():  # Test for the division function
+def test_div():
     assert div(1, 1) == 1
     assert div(-60, 4) == -15
     assert div(-4, -1) == 4
