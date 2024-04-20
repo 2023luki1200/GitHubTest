c375960b8bde272dfcfc568a2ed4846d94939ed2
Dominik Luther <domi.luther@gmail.com>
2024-04-20 11:06:01 +0200

    adapt to fail tests

--- test_calc.py
+++ test_calc.py
@@ -2,7 +2,7 @@ from calc import call_func
 
 
 def test_call_func():
-    assert call_func("a", 1, 1) == 2
+    assert call_func("a", 1, 1) ==5
     assert call_func("s", 13, 5) == 8
     assert call_func("m", 1, 2) == 2
     assert call_func("d", 4, 2) == 2
